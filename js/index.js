8b48051ae145abc0bd685f85c0bd814522248cb1
drdexter <dr.dexter.ua@gmail.com>
2017-05-17 22:48:10 +0000

    added animation

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 $(function(){
     
     $('#country-select').on('change', function(){
-        console.log('click');
-        $('#country-dropdown').toggle();
-        $('#city-dropdown').toggle().addClass('center');
+        // $('body, html').addClass('overflow-hidden');
+        $('#country-dropdown').hide("slide", { direction: "left", distance: 2000 }, 1000);
+        $('#city-dropdown').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
+        // $('body, html').removeClass('overflow-hidden');
     });
     
     $('#city-select').on('change', function(){
-        console.log('click');
-        $('#city-dropdown').toggle();
-        $('#wheather-button').toggle().addClass('center');
+        // $('body, html').addClass('overflow-hidden');
+        $('#city-dropdown').hide("slide", { direction: "left", distance: 2000 }, 1000);
+        $('#button-main-div').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
+        // $('body, html').removeClass('overflow-hidden');
     });
     
 });
