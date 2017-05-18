a0265e63749daba66e1075c688380e817f992212
drdexter <dr.dexter.ua@gmail.com>
2017-05-18 00:06:05 +0000

    added button

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 $(function(){
     
     $('#country-select').on('change', function(){
-        // $('body, html').addClass('overflow-hidden');
         $('#country-dropdown').hide("slide", { direction: "left", distance: 2000 }, 1000);
         $('#city-dropdown').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
-        // $('body, html').removeClass('overflow-hidden');
+    });
+    
+    $('option').on('click', function(){
+        console.log('click');
+        $('#country-dropdown').hide("slide", { direction: "left", distance: 2000 }, 1000);
+        $('#city-dropdown').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
     });
     
     $('#city-select').on('change', function(){
-        // $('body, html').addClass('overflow-hidden');
         $('#city-dropdown').hide("slide", { direction: "left", distance: 2000 }, 1000);
         $('#button-main-div').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
-        // $('body, html').removeClass('overflow-hidden');
+    });
+    
+    $('#home-li').on('click', function(e) {
+        $('#city-dropdown').hide("slide", { direction: "left", distance: 2000 }, 1000);
+        $('#button-main-div').hide("slide", { direction: "left", distance: 2000 }, 1000);
+        $('#country-dropdown').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
     });
     
 });
