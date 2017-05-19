013ead56d1d39c8ffea473d2f279d649e3d6e7da
drdexter <dr.dexter.ua@gmail.com>
2017-05-19 11:30:19 +0000

    implemented table

---
+++ js/weather.js
@@ -0,0 +1,19 @@
+// $("#btn").click(function(){
+//     var geocoder =  new google.maps.Geocoder();
+//     geocoder.geocode( { 'address': 'miami, us'}, function(results, status) {
+//     if (status == google.maps.GeocoderStatus.OK) {
+//       alert("location : " + results[0].geometry.location.lat() + " " +results[0].geometry.location.lng()); 
+//     } else {
+//       alert("Something got wrong " + status);
+//     }
+//   });
+// });
+
+
+$(function(){
+  
+  
+  
+  
+  
+});
