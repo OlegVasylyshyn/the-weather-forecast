92a06c140040bf9fc830e2de3182e85f302276d7
drdexter <dr.dexter.ua@gmail.com>
2017-05-19 08:30:29 +0000

    fixed selects

--- js/index.js
+++ js/index.js
@@ -5,10 +5,10 @@ $(function(){
         $('#city-dropdown').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
     });
     
-    $('option').on('click', function(){
-        console.log('click');
-        $('#country-dropdown').hide("slide", { direction: "left", distance: 2000 }, 1000);
-        $('#city-dropdown').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
+    $('#country-dropdown').on('loaded.bs.select', function() {
+        $("#country-dropdown div div ul li").click(function(e){
+            $('#country-select').change();
+        });
     });
     
     $('#city-select').on('change', function(){
@@ -16,7 +16,14 @@ $(function(){
         $('#button-main-div').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
     });
     
+    $('#city-dropdown').on('loaded.bs.select', function() {
+        $("#city-dropdown div div ul li").click(function(e){
+            $('#city-select').change();
+        });
+    });
+    
     $('#home-li').on('click', function(e) {
+        e.preventDefault(); 
         $('#city-dropdown').hide("slide", { direction: "left", distance: 2000 }, 1000);
         $('#button-main-div').hide("slide", { direction: "left", distance: 2000 }, 1000);
         $('#country-dropdown').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
