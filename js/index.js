8c76105d43b67345c3d141a9bda40574c6ae0340
drdexter <dr.dexter.ua@gmail.com>
2017-05-19 10:05:33 +0000

    created weather page

--- js/index.js
+++ js/index.js
@@ -30,4 +30,14 @@ $(function(){
         $('#country-dropdown').show("slide", { direction: "right", distance: 2000 }, 1000).addClass('center');
     });
     
+    $('#weather-button').on('click', function() {
+        // TODO 
+        // 1) Take all parameters from selected options
+        // 2) Persist them in sessionStorage
+        // 3) Change page and use these parameters on weather page
+        
+        window.location='./weather.html';
+        
+    })
+    
 });
